f93e0704d5d52d68164aa43cd5e7f03a00d4794d
Antony Cen <antony.cen@correo.unimet.edu.ve>
2025-01-20 01:34:25 -0400

    Add files via upload

---
+++ src/Funciones/Funcion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funciones;
+
+/**
+ *
+ * @author 58412
+ */
+public class Funcion {
+    
+    public static void Sumar(int num1,int num2){
+        
+        int var1=num1;
+        int var2=num2;
+        int suma = var1+ var2;
+        System.out.println(suma);
+    }
+       
+}
